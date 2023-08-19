70ac7ffc71c77e3c0976770197f332083d3e7702
gabrielestefono <gabrielestefono@hotmail.com>
2023-08-19 10:56:18 -0300

    Conectando função ao evento botão

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,29 @@ import 'package:flutter/material.dart';
 main() => runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
+  void responder() {
+    print('Pergunta Respondida!');
+  }
+
   @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      "Qual é a sua cor favorita?",
+      "Qual é o seu animal favorito?"
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Perguntas"),
         ),
-        body: Text("Olá Flutter!!!"),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            ElevatedButton(onPressed: responder(), child: Text("Resposta 1")),
+            ElevatedButton(onPressed: null, child: Text("Resposta 2")),
+            ElevatedButton(onPressed: null, child: Text("Resposta 3"))
+          ],
+        ),
       ),
     );
   }
