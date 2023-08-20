95cc43b4e50ddec770e6b2b64c95166cfd9605a4
gabrielestefono <gabrielestefono@hotmail.com>
2023-08-20 17:02:57 -0300

    Evitar chegar na pergunta inválida

--- lib/main.dart
+++ lib/main.dart
@@ -6,48 +6,53 @@ main() => runApp(PerguntaApp());
 
 class _PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
+  final _perguntas = const [
+    {
+      "texto": "Qual é a sua cor favorita?",
+      "respostas": [
+        "Verde",
+        "Vermelho",
+        "Azul",
+        "Preto",
+      ],
+    },
+    {
+      "texto": "Qual é o seu animal favorito?",
+      "respostas": [
+        "Coelho",
+        "Cobra",
+        "Elefante",
+        "Leão",
+      ],
+    },
+    {
+      "texto": "Qual é o seu professor favorito?",
+      "respostas": [
+        "Maria",
+        "Leo",
+        "Gabriel",
+        "João",
+      ],
+    }
+  ];
 
   void _responder() {
-    setState(() {
-      _perguntaSelecionada++;
-    });
-    print(_perguntaSelecionada);
+    if (temPerguntaSelecionada) {
+      setState(() {
+        _perguntaSelecionada++;
+      });
+    }
+  }
+
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
   }
 
   @override
   Widget build(BuildContext context) {
-    final List<Map<String, Object>> perguntas = [
-      {
-        "texto": "Qual é a sua cor favorita?",
-        "respostas": [
-          "Verde",
-          "Vermelho",
-          "Azul",
-          "Preto",
-        ],
-      },
-      {
-        "texto": "Qual é o seu animal favorito?",
-        "respostas": [
-          "Coelho",
-          "Cobra",
-          "Elefante",
-          "Leão",
-        ],
-      },
-      {
-        "texto": "Qual é o seu professor favorito?",
-        "respostas": [
-          "Maria",
-          "Leo",
-          "Gabriel",
-          "João",
-        ],
-      }
-    ];
-
-    List<String> respostas =
-        perguntas[_perguntaSelecionada].cast()["respostas"];
+    List<String> respostas = temPerguntaSelecionada
+        ? _perguntas[_perguntaSelecionada].cast()["respostas"]
+        : [];
 
     return MaterialApp(
       home: Scaffold(
@@ -57,20 +62,24 @@ class _PerguntaAppState extends State<PerguntaApp> {
           ),
           centerTitle: true,
         ),
-        body: Column(
-          children: [
-            Questao(
-              perguntas[_perguntaSelecionada].cast()["texto"],
-            ),
-            ...respostas.map((t) => Resposta(t, _responder)).toList()
-          ],
-        ),
+        body: temPerguntaSelecionada
+            ? Column(
+                children: [
+                  Questao(
+                    _perguntas[_perguntaSelecionada].cast()["texto"],
+                  ),
+                  ...respostas.map((t) => Resposta(t, _responder)).toList()
+                ],
+              )
+            : Container(),
       ),
     );
   }
 }
 
 class PerguntaApp extends StatefulWidget {
+  const PerguntaApp({super.key});
+  @override
   _PerguntaAppState createState() {
     return _PerguntaAppState();
   }
