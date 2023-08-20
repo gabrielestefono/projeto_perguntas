e64c7ed3ab15bc8e2fcab1d6b7d2a521278820af
gabrielestefono <gabrielestefono@hotmail.com>
2023-08-19 22:44:00 -0300

    Corrigindo, era o container

--- lib/resposta.dart
+++ lib/resposta.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 
 class Resposta extends StatelessWidget {
-  String textoResposta;
-  Resposta(this.textoResposta, {super.key});
+  final String texto;
+  Resposta(this.texto, {super.key});
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const ElevatedButton(
-        onPressed: null,
-        child: Text(textoResposta),
-      ),
+    return ElevatedButton(
+      onPressed: null,
+      child: Text(texto),
     );
   }
 }
