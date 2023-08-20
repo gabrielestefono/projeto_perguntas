e50d8df1527433564f0d36f404f85c91e49d9812
gabrielestefono <gabrielestefono@hotmail.com>
2023-08-19 22:11:21 -0300

    Aplicando estilo ao Widget Questão

--- lib/questao.dart
+++ lib/questao.dart
@@ -5,6 +5,16 @@ class Questao extends StatelessWidget {
   Questao(this.texto);
   @override
   Widget build(BuildContext context) {
-    return Text(texto);
+    return Container(
+      child: Text(
+        texto,
+        style: TextStyle(
+          fontSize: 28,
+        ),
+        textAlign: TextAlign.center,
+      ),
+      width: double.infinity,
+      margin: EdgeInsets.all(20),
+    );
   }
 }
