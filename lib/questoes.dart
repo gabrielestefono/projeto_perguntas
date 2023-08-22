f631dab154e350a549eff30e092ae0bdad594a13
gabrielestefono <gabrielestefono@hotmail.com>
2023-08-21 21:49:11 -0300

    Passando a resposta para um novo componente

---
+++ lib/questoes.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Questoes extends StatelessWidget {
+  const Questoes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
